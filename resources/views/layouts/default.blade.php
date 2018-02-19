dc7339f91de4797d5d668b1da2b3d495a748dffe
jyostna <ju36@njit.edu>
2018-02-19 02:24:36 -0500

    Jumbotron, Nav menu, footer are set

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+    @include('includes.head')
+    @include('includes.menu')
+<body>
+
+<main role="main">
+    @yield('content')
+</main>
+
+    @include('includes.jsfooter')
+    @include('includes.footer')
+</body>
+</html>
