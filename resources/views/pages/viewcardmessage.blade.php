5459137c2f39a294ba9213984f269df99c33a8a3
jyostna <ju36@njit.edu>
2018-02-24 23:16:11 -0500

    Added Content in Bootstrap card

--- resources/views/pages/viewcardmessage.blade.php
+++ resources/views/pages/viewcardmessage.blade.php
@@ -7,7 +7,14 @@
         <div class="card-block ml-3 mt-3">
             <h4 class="card-title">Message</h4>
             <p class="card-text">This is the static content for now. Redirecting to respective pages (dynamically) will be done after DB is covered</p>
-            <a href="/viewmessages" class="btn btn-primary">View Messages</a>
+
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Subject:</li>
+                    <li class="list-group-item">From Email:</li>
+                    <li class="list-group-item">Message:</li>
+                </ul>
+            </div>
+
         </div>
-    </div>
+    <a href="/viewmessages" class="btn btn-primary mt-2">View Messages</a>
 @endsection
